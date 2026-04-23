e0bc5c24c40a75c34686a7152253fadeb8e8d7dd
Ali Duman <alidumanpr@hotmail.com>
2026-04-23 20:10:42 +0300

    scroll behavior fix

--- src/components/MovieChat.jsx
+++ src/components/MovieChat.jsx
@@ -104,6 +104,18 @@ function ChatPanel({ movieId, movieTitle, onClose, isMobile }) {
   }, [isMobile]);
 
   useEffect(() => {
+    if (!isMobile) return;
+    document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.width = '100%';
+    return () => {
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
+    };
+  }, [isMobile]);
+
+  useEffect(() => {
     markSeen();
     inputRef.current?.focus();
     return () => closeSeen();
